5b9a095f1751a0ae65124a8233a2432189370fd2
Sushil-Deore <sushildeore99@gmail.com>
2021-11-15 12:27:14 +0530

    updating UI and adding logging to code

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import joblib
+import logging
+
+# Application logging
+# Configuring logging operations
+
+logging.basicConfig(filename='app_deployment_logs.log', level=logging.INFO,
+                    format='%(levelname)s:%(asctime)s:%(message)s')
 
 # Create Flask object to Run
 app = Flask(__name__)
@@ -8,6 +15,8 @@ app = Flask(__name__)
 # Load the model from the File
 
 model_load = joblib.load('./model/premium_pred_model.pkl')
+
+logging.info('Pickle file loading completed.')
 
 
 @app.route('/')
@@ -37,8 +46,13 @@ def predict():
         final_features = [np.array(input_val)]
 
         output = model_load.predict(final_features).tolist()
-        
-        return render_template('index.html', prediction_text=f'Insurance Premium is {output}')
+
+        # logging operation
+        logging.info(f"Insurance Premium is {output}")
+
+        logging.info('Prediction getting posted to the web page.')
+
+        return render_template('index.html', prediction_text=f'Insurance Premium is $ {output} ')
     else:
         return render_template('index.html')
 
